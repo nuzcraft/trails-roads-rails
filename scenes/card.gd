5ae704c307918b2d27421f0f27210251c5cb47da
nuzcraft <nuzcraft@gmail.com>
2024-12-04 21:38:50 -0600

    cards can now affect the grid

--- scenes/card.gd
+++ scenes/card.gd
@@ -1,23 +1,28 @@
 extends Control
 class_name Card
 
+signal card_dropped(card, atlas_position)
+
 var return_pos: Vector2
 
 enum STATE {
 	STATIC,
-	FOLLOWING
+	FOLLOWING,
+	FROZEN
 }
 
 var current_state: int =  STATE.STATIC
+var atlas_pos: Vector2
 
 func _ready() -> void:
 	return_pos = position
-
+	var tex: AtlasTexture = $PanelContainer/VBoxContainer/TextureRect.texture
+	atlas_pos = tex.region.position / 16
+	
 func _process(delta: float) -> void:
 	match current_state:
 		STATE.STATIC:
 			modulate.a = 1.0
-			pass
 		STATE.FOLLOWING:
 			position = round(get_global_mouse_position() - (size / 4))
 			modulate.a = 0.5
@@ -26,25 +31,23 @@ func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
-				print("Left mouse button")
-				current_state = STATE.FOLLOWING
-			#MOUSE_BUTTON_RIGHT:
-				#print("right mouse button")
-				#var tween = get_tree().create_tween()
-				#tween.set_ease(Tween.EASE_IN_OUT)
-				#tween.tween_property(self, "position", return_pos, 0.2)
-				#current_state = STATE.STATIC
+				switch_state(STATE.FOLLOWING)
+			MOUSE_BUTTON_RIGHT:
+				switch_state(STATE.STATIC)
 	elif event is InputEventMouseButton and not event.pressed:
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
-				print("Left mouse button released")
-				var tween = get_tree().create_tween()
-				tween.set_ease(Tween.EASE_IN_OUT)
-				tween.tween_property(self, "position", return_pos, 0.2)
-				current_state = STATE.STATIC
-			#MOUSE_BUTTON_RIGHT:
-				#print("right mouse button")
-				#var tween = get_tree().create_tween()
-				#tween.set_ease(Tween.EASE_IN_OUT)
-				#tween.tween_property(self, "position", return_pos, 0.2)
-				#current_state = STATE.STATIC
+				if current_state == STATE.FOLLOWING:
+					card_dropped.emit(self, atlas_pos)
+					switch_state(STATE.FROZEN)
+				#switch_state(STATE.STATIC)
+
+func switch_state(state: int) -> void:
+	match state:
+		STATE.STATIC:
+			var tween = get_tree().create_tween()
+			tween.set_ease(Tween.EASE_IN_OUT)
+			tween.tween_property(self, "position", return_pos, 0.2)
+			current_state = state
+		_:
+			current_state = state
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,20 @@
 extends Node
 
+@onready var tile_map_layer_feature: TileMapLayer = $Control/SubViewportContainer/SubViewport/TileMapLayerFeature
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Control/Card.card_dropped.connect(on_card_dropped)
+	$Control/Card2.card_dropped.connect(on_card_dropped)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func on_card_dropped(card: Card, atlas_position: Vector2) -> void:
+	print("card dropped heh")
+	print(tile_map_layer_feature.get_local_mouse_position())
+	print(tile_map_layer_feature.local_to_map(tile_map_layer_feature.get_local_mouse_position()))
+	var cell_coord: Vector2i = tile_map_layer_feature.local_to_map(tile_map_layer_feature.get_local_mouse_position())
+	tile_map_layer_feature.set_cell(cell_coord, 0, atlas_position)
+	card.queue_free()
